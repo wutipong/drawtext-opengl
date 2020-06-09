0c2c06c42949684e671f31671296c40bc57b1c2f
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2020-06-09 17:56:46 +0700

    update the window title.

--- main.cpp
+++ main.cpp
@@ -75,8 +75,8 @@ int main(int, char **) {
   SDL_WindowFlags window_flags = (SDL_WindowFlags)(
       SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
   SDL_Window *window = SDL_CreateWindow(
-      "Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+      "DrawText sample using OpenGL, FreeType2, Harfbuzz.",
+      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
   SDL_GLContext gl_context = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, gl_context);
   SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -176,7 +176,6 @@ int main(int, char **) {
     glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    
     Scene::TickCurrent(context);
 
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
