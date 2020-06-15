52bf9a610c949e857a25d050dcd36e79b7a4247c
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2020-06-16 01:25:40 +0700

    lowering the OpenGL version to 4.5

--- main.cpp
+++ main.cpp
@@ -59,13 +59,13 @@ int main(int, char **) {
     return -1;
   }
 
-  const char *glsl_version = "#version 460";
+  const char *glsl_version = "#version 450";
   SDL_GL_SetAttribute(
       SDL_GL_CONTEXT_FLAGS,
       SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
   // Create window with graphics context
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
